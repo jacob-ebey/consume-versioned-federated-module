b8880e508e59d7b816a0d8b3e65d5e7acc91b2f2
Jacob Ebey <jacob.ebey@live.com>
2021-01-13 22:24:57 -0800

    feat(versioned-federated-module): bumped to consume new feature

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,6 @@
 import logHello from "versioned-federated-module/utils/log-hello";
+import version from "versioned-federated-module/utils/version";
 
 logHello("Consumer");
+
+console.log(`versioned-federated-module@${version()}`);
